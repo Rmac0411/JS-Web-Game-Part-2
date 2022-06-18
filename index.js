35f8350c814c08b8ee9ca6e35a93b74f31c2cc70
rmac0411 <rmac0411@gmail.com>
2022-06-17 22:16:51 -0700

    first commit

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function newItem(url, left, bottom){
     })
 }
 
-const inventory = newInventory()
+let inventory = document.createElement('div')
 
 function newInventory(){
-    let inventory = document.createElement('div')
+    
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
